b6e47b0ee405179c5bbb729cca4a9c1154c09242
alumno <alumno@macbook-de-admin-2.local>
2017-03-02 21:30:59 -0500

    terminar

---
+++ EjercicioClase3Modulo2/CeldaCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CeldaCollectionViewCell.swift
+//  EjercicioClase3Modulo2
+//
+//  Created by alumno on 2/03/17.
+//  Copyright © 2017 alg. All rights reserved.
+//
+
+import UIKit
+
+class CeldaCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ EjercicioClase3Modulo2/GrillaViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GrillaViewController.swift
+//  EjercicioClase3Modulo2
+//
+//  Created by alumno on 2/03/17.
+//  Copyright © 2017 alg. All rights reserved.
+//
+
+import UIKit
+
+class GrillaViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+}
